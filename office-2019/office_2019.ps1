d40df504bee502ae93afe2a83927d75f90c8850e
Cryozyme <64743046+Cryozyme@users.noreply.github.com>
2024-01-15 13:11:26 -0600

    Dependencies have been added to the download queue.

--- office-2019/office_2019.ps1
+++ office-2019/office_2019.ps1
@@ -1,12 +1,68 @@
-function configDownload() {
-
-    $script_base_url = "https://raw.githubusercontent.com/Cryozyme/office-deployment/main/office-2019"
+function dependenciesDownload() {
 
     try {
         
-        Invoke-RestMethod -Uri "$script_base_url/xml-config/bits-transfer.csv" -UseBasicParsing -OutFile "$deployment_tool_path\bits-transfer.csv"
-        Import-Csv -Path "$deployment_tool_path\bits-transfer.csv" -Delimiter "," | Start-BitsTransfer -TransferType Download -Priority Foreground
-        Remove-Item -Path "$deployment_tool_path\bits-transfer.csv" -Force
+        $app_id = 49117
+        $url_base = "https://www.microsoft.com/en-us/download/confirmation.aspx"
+        $odt_version = "$url_base`?id=$app_id"
+        $latest = Invoke-WebRequest -UseBasicParsing $odt_version
+        $file_pattern = "officedeploymenttool_"
+        $version = (($latest.Links | Where-Object {$_.href -match "$file_pattern"}).href).Split("`n")[0].Trim()
+        $file_name = "odt2019.exe"
+        
+        Write-Host "Downloading Office Deployment Tool from $version"
+        Start-BitsTransfer -Source $version -Destination "$deployment_tool_path\$file_name" -TransferType Download -Priority Foreground
+
+    } catch {
+        
+        Write-Host "$_"
+
+    } try {
+        
+        if(-not(Test-Path -Path "$env:ProgramFiles\7-Zip" -PathType Container)) {
+            
+            $url_base = "https://7-zip.org/download.html"
+            $latest = Invoke-WebRequest -UseBasicParsing $url_base
+            $file_pattern = "-x64.exe"
+            $version = (($latest.Links | Where-Object {$_.href -match "$file_pattern"}).href).Split(" ")[0].Trim()
+            $version = ($url_base.Split("download.html")[0].Trim() + $version)
+            $file_name = "7zip.exe"
+            
+            Write-Host "Downloading 7-zip from $version"
+            Start-BitsTransfer -Source $version -Destination "$deployment_tool_path\$file_name" -TransferType Download -Priority Foreground
+
+        } else {
+
+            throw "7-Zip is Already Installed"
+
+        }
+
+    } catch {
+
+        Write-Host "$_"
+
+    } finally {
+
+        Exit-PSHostProcess
+        Exit-PSSession
+
+    }
+
+    return
+
+}
+function configDownload() {
+
+    $bits_transfer = "https://raw.githubusercontent.com/Cryozyme/office-deployment/main/office-2019/xml-config/configuration.csv"
+
+    try {
+        
+        Write-Host "Downloading Configuration Files from $bits_transfer"
+        Invoke-WebRequest -UseBasicParsing "$bits_transfer" -OutFile "$deployment_tool_path\configuration.csv"
+        Set-Location -Path "$deployment_tool_path"
+        Import-Csv -Path "$deployment_tool_path\configuration.csv" -Delimiter "," | Start-BitsTransfer -TransferType Download -Priority Foreground
+        Set-Location -Path "-"
+        Remove-Item -Path "$deployment_tool_path\configuration.csv" -Force
 
     } catch {
 
@@ -14,6 +70,7 @@ function configDownload() {
 
     }
 
+    dependenciesDownload
     return
 
 }
@@ -23,7 +80,6 @@ function fileHandling() {
 
     #Variables
     $deployment_tool_path = "$env:homedrive\office_deployment\odt_2019"
-    #$cpu_architecture = "$env:processor_architecture"
 
     try {
         
